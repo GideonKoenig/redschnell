740eca53a0fd81299ba931acbf32d708e3fbbd9f
Gideon Koenig <gideon.koenig@online.de>
2024-10-27 19:26:24 +0100

    meta: test build process

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -11,6 +11,7 @@ import {
     CollapsibleContent,
     CollapsibleTrigger,
 } from "~/components/ui/collapsible";
+import { UploadProgressProvider } from "~/uploadthing/upload-progress-provider";
 
 export default async function RootLayout({
     children,
@@ -24,76 +25,78 @@ export default async function RootLayout({
         <>
             <ThemeToggle className="absolute left-0 top-0" />
             <UploadModal />
-            <Tabs defaultValue="files" className="w-full">
-                <Collapsible defaultOpen asChild>
-                    <main className="grid h-screen w-screen grid-cols-[22rem_auto] gap-4 bg-menu-main p-4 data-[state=closed]:grid-cols-1 dark:bg-dark-menu-main">
-                        <CollapsibleContent asChild>
-                            <div className="h-14 rounded-lg border border-menu-hover bg-menu-light p-1 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
-                                <TabsList className="h-full w-full bg-menu-light p-0">
-                                    <TabsTrigger value="files">
-                                        Your files
-                                    </TabsTrigger>
-                                    <TabsTrigger value="chats">
-                                        Your chats
-                                    </TabsTrigger>
-                                    <CollapsibleTrigger asChild>
-                                        <Button variant="ghost">
-                                            <PanelRightOpen />
-                                        </Button>
-                                    </CollapsibleTrigger>
-                                </TabsList>
+            <UploadProgressProvider>
+                <Tabs defaultValue="files" className="w-full">
+                    <Collapsible defaultOpen asChild>
+                        <main className="grid h-screen w-screen grid-cols-[22rem_auto] gap-4 bg-menu-main p-4 data-[state=closed]:grid-cols-1 dark:bg-dark-menu-main">
+                            <CollapsibleContent asChild>
+                                <div className="h-14 rounded-lg border border-menu-hover bg-menu-light p-1 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
+                                    <TabsList className="h-full w-full bg-menu-light p-0">
+                                        <TabsTrigger value="files">
+                                            Your files
+                                        </TabsTrigger>
+                                        <TabsTrigger value="chats">
+                                            Your chats
+                                        </TabsTrigger>
+                                        <CollapsibleTrigger asChild>
+                                            <Button variant="ghost">
+                                                <PanelRightOpen />
+                                            </Button>
+                                        </CollapsibleTrigger>
+                                    </TabsList>
+                                </div>
+                            </CollapsibleContent>
+                            <div className="flex h-14 w-full flex-row items-center gap-2 rounded-lg border border-menu-hover bg-menu-main p-2 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
+                                <CollapsibleTrigger asChild>
+                                    <Button
+                                        variant="ghost"
+                                        className="data-[state=open]:hidden"
+                                    >
+                                        <PanelRightClose />
+                                    </Button>
+                                </CollapsibleTrigger>
+                                <ActionBar />
                             </div>
-                        </CollapsibleContent>
-                        <div className="flex h-14 w-full flex-row items-center gap-2 rounded-lg border border-menu-hover bg-menu-main p-2 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
-                            <CollapsibleTrigger asChild>
-                                <Button
-                                    variant="ghost"
-                                    className="data-[state=open]:hidden"
-                                >
-                                    <PanelRightClose />
-                                </Button>
-                            </CollapsibleTrigger>
-                            <ActionBar />
-                        </div>
-                        <CollapsibleContent asChild>
-                            <div className="h-full w-[22rem] rounded-lg border border-menu-hover bg-menu-main shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
-                                <ScrollArea className="h-[calc(100vh-6.5rem)]">
-                                    <TabsContent value="files">
-                                        <div className="flex w-full flex-col gap-2 p-4">
-                                            {files.map((file, index) => (
-                                                <div
-                                                    key={index}
-                                                    data-state={index}
-                                                    className="cursor-pointer rounded-lg p-2 hover:bg-menu-light data-[state=1]:bg-menu-light dark:hover:bg-dark-menu-light dark:data-[state=1]:bg-dark-menu-light"
-                                                >
-                                                    {file.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </TabsContent>
-                                    <TabsContent value="chats">
-                                        <div className="flex w-full flex-col gap-2 p-4">
-                                            {chats.map((chat, index) => (
-                                                <div
-                                                    key={index}
-                                                    data-state={index}
-                                                    className="cursor-pointer rounded-lg p-2 hover:bg-menu-light data-[state=1]:bg-menu-light dark:hover:bg-dark-menu-light dark:data-[state=1]:bg-dark-menu-light"
-                                                >
-                                                    {chat.name}
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </TabsContent>
-                                </ScrollArea>
-                            </div>
-                        </CollapsibleContent>
+                            <CollapsibleContent asChild>
+                                <div className="h-full w-[22rem] rounded-lg border border-menu-hover bg-menu-main shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
+                                    <ScrollArea className="h-[calc(100vh-6.5rem)]">
+                                        <TabsContent value="files">
+                                            <div className="flex w-full flex-col gap-2 p-4">
+                                                {files.map((file, index) => (
+                                                    <div
+                                                        key={index}
+                                                        data-state={index}
+                                                        className="cursor-pointer rounded-lg p-2 hover:bg-menu-light data-[state=1]:bg-menu-light dark:hover:bg-dark-menu-light dark:data-[state=1]:bg-dark-menu-light"
+                                                    >
+                                                        {file.name}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </TabsContent>
+                                        <TabsContent value="chats">
+                                            <div className="flex w-full flex-col gap-2 p-4">
+                                                {chats.map((chat, index) => (
+                                                    <div
+                                                        key={index}
+                                                        data-state={index}
+                                                        className="cursor-pointer rounded-lg p-2 hover:bg-menu-light data-[state=1]:bg-menu-light dark:hover:bg-dark-menu-light dark:data-[state=1]:bg-dark-menu-light"
+                                                    >
+                                                        {chat.name}
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </TabsContent>
+                                    </ScrollArea>
+                                </div>
+                            </CollapsibleContent>
 
-                        <div className="h-[calc(100vh-6.5rem)] w-full rounded-lg border border-menu-hover bg-menu-main p-4 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
-                            {children}
-                        </div>
-                    </main>
-                </Collapsible>
-            </Tabs>
+                            <div className="h-[calc(100vh-6.5rem)] w-full rounded-lg border border-menu-hover bg-menu-main p-4 shadow dark:border-dark-menu-light dark:bg-dark-menu-main">
+                                {children}
+                            </div>
+                        </main>
+                    </Collapsible>
+                </Tabs>
+            </UploadProgressProvider>
         </>
     );
 }
