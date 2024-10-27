5915d84f1e13186d587c689d5a3b09af4a9e98b1
GideonKoenig <gideon.koenig@online.de>
2024-10-27 13:01:27 +0100

    feat: make sidebar collapsible

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -6,7 +6,7 @@ export default async function App() {
             <Icon className="inline-block h-48 w-48 text-menu-hover dark:text-dark-menu-light" />
             <div className="flex flex-col items-center gap-6 rounded-lg border-[4px] border-dashed border-menu-light p-6 dark:border-dark-menu-light">
                 <p className="text-lg">
-                    <span className="font-bold text-accent-light/70 dark:text-dark-accent-light/70">
+                    <span className="font-bold text-accent-main/70 dark:text-dark-accent-light/70">
                         Upload
                     </span>
                     <span>
@@ -15,7 +15,7 @@ export default async function App() {
                 </p>
                 <p>or</p>
                 <p className="text-lg">
-                    <span className="font-bold text-accent-light/70 dark:text-dark-accent-light/70">
+                    <span className="font-bold text-accent-main/70 dark:text-dark-accent-light/70">
                         Create
                     </span>
                     <span>
